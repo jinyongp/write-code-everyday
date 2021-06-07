10e243e79141ff4e86c2806514c95a6c704fae7c
jinyongp <dev.jinyongp@gmail.com>
2021-06-07 09:44:03 +0900

    Update comments

--- programmers/PART18/구명보트.js
+++ programmers/PART18/구명보트.js
@@ -11,44 +11,58 @@
  *
  * 목표: 구명보트를 최대한 적게 사용
  *
- * 한 명을 태우고 뒤를 전부 검사하여 함께 탈 수 있는 사람 여부를 확인한다. => 50000^2 = 25억 시간 초과
+ * weight를 낮은 몸무게의 사람들과 무거운 사람들을 같이 태우기 위해 오름차순으로 정렬한다.
  *
- * answer = 0
- *
- * weight를 오름차순으로 sort한다.
- *
- * [50, 50, 70, 80];
- *
- * 80을 뺀다.
- * 80을 50과 합해서 limit 이내면 50을 뺀다. 아니므로 빼지 않는다.
- *
- * people을 모두 계산할 때까지 반복한다.
+ * 구명보트이 개수: answer = 0
+ * leftIndex와 rightIndex를 선언하여 무거운 사람들과 가벼운 사람들이 같이 탑승할 수 있는지 검사한다.
+ * - leftIndex과 rightIndex보다 커질 때까지 반복한다.
+ * - leftIndex에는 가장 몸무게가 낮은 사람, rightIndex에는 가장 몸무게가 큰 사람이 있으므로, 둘이 합해서 limit을 넘지 않으면 leftIndex를 1 증가한다.(가벼운 사람이 동승할 수 있다는 의미)
+ * - 가벼운 사람의 동승과 관계없이 무거운 사람은 구명 보트 하나를 차지하므로 rightIndex를 1 감소한다.
+ * - 한 번의 반복으로 보트가 추가되므로 answer + 1
+ * - 만약, 가벼운 사람(leftIndex)이 limit의 절반값보다 커지게 된다면, 더 이상 무거운 사람과 동승할 수 없으므로 반복문을 종료하여 인당 보트 1개로 계산하도록 한다.
+ * - answer와 남은 사람을 합하여 반환한다.
  */
+function solution(people, limit) {
+  people.sort((a, b) => a - b);
+  let answer = 0;
+  let leftIndex = 0;
+  let rightIndex = people.length - 1;
+
+  // while (people.length) {
+  //   if (people.pop() + people[0] <= limit) {
+  //     people.shift();
+  //   }
+  //   answer += 1;
+  //   if (people[0] > limit / 2) break;
+  // }
+
+  // return answer + people.length;
+
+  // pop이나 shift와 같은 메서드 호출 방법보다 index 조작이 더 빠르다.
+  while (leftIndex <= rightIndex) {
+    const totalWeight = people[rightIndex] + people[leftIndex];
+    if (totalWeight <= limit) leftIndex += 1;
+    rightIndex -= 1;
+    answer += 1;
+
+    if (people[leftIndex] > limit / 2) break;
+  }
+
+  const remain = people.slice(leftIndex, rightIndex + 1);
+  return answer + remain.length;
+}
+
 // function solution(people, limit) {
 //   let answer = 0;
 //   people.sort((a, b) => a - b);
 
-//   while (people.length) {
-//     if (people.pop() + people[0] <= limit) {
-//       people.shift();
-//     }
+//   const { length } = people;
+//   for (let left = 0, right = length - 1; left <= right; right -= 1) {
+//     if (people[left] + people[right] <= limit) left += 1;
 //     answer += 1;
-//     if (people[0] > limit / 2) break;
 //   }
-//   return answer + people.length;
+//   return answer;
 // }
-
-function solution(people, limit) {
-  let answer = 0;
-  people.sort((a, b) => a - b);
-
-  const { length } = people;
-  for (let left = 0, right = length - 1; left <= right; right -= 1) {
-    if (people[left] + people[right] <= limit) left += 1;
-    answer += 1;
-  }
-  return answer;
-}
 
 let people, limit;
 
--- programmers/PART18/조이스틱.js
+++ programmers/PART18/조이스틱.js
@@ -6,45 +6,43 @@
  * ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
  * ▶ - 커서를 오른쪽으로 이동
  *
- * 탐욕법 문제이므로 당장 이동해야할 곳을 찾고 그 최솟값을 비교하여 진행한다.
+ * 탐욕법 문제이므로 당장 이동해야 할 곳을 찾고 그 최솟값을 비교하여 진행한다.
  *
- * JAZ
+ * name = "JAZ"
  *
- * 배열을 만들어서 각 자리에 상하로 이동해야하는 최소 횟수를 저장한다.
- * [9, 0, 1] <- min("J" - "A", "Z" - "J" + 1)
+ * 배열을 만들어서 각 자리에 상하로 이동해야하는 최소 횟수를 저장한다. (▲, ▼ 최소 횟수)
+ * ex) minCount = [9, 0, 1] <- min("J" - "A", "Z" - "J" + 1)
  *
- * 배열의 sum값을 count의 초기값으로 한다. count는 구해야하는 최소 이동 횟수이다.
- *
- * 인덱스를 0부터 시작하여 0이 아닌 수를 count에 더하고 배열의 값을 0으로 한 뒤 index를 +1한다.
- *
- * index가 1일 때 0이므로 우측으로 이동해서 0이 아닌 수를 만나는 것이 빠를 지 아니면 좌촉으로 이동해서 0이 아닌 수를 만나는 것이 빠른지 계산한다.
- *
- * min(우측으로 이동해서 0이 아닌 수를 만나는 횟수, 좌측으로 이동해서 0이 아닌 수를 만나는 횟수)
- *
- * 최소 횟수를 count에 더하고 좌측으로 가는 횟수가 많으면 index에서는 빼고 (음수를 다시 양수로), 우측으로 가는 횟수가 많으면 index에서는 더한다.
- *
- * 현재 인덱스가 1이니까 우측으로 이동하는 횟수는 1이고, 좌측으로 이동하는 횟수는 2이므로 1을 선택하여 count에 1을 더하고 index에도 1을 더한다.
- *
- * 이동한 위치부터 반복을 다시 시작한다.
+ * ▲, ▼ 방향에서의 최소 조작 횟수가 구해졌으므로 ◀, ▶ 방향의 최소 조작 횟수를 계산해야 한다.
+ * - 왼쪽 방향으로 움직일 leftIndex, 오른쪽 방향으로 움직일 rightIndex를 선언한다.
+ * - 각 인덱스는 해당 방향으로 움직이면서 0이 아닌 구간을 만날 때까지 이동한다.
+ * - 이동한 구간이 더 짧은 인덱스를 선택하여 count에 추가한 후, 그 방향으로 현재 커서 위치를 옮긴다.
+ * - 옮긴 위치에서 minCount를 통해 현재 커서 위치의 ▲, ▼ 방향에서의 최소 조작 횟수를 더하고 그 부분은 0으로 초기화한다.
+ * - minCount의 총합이 0이 되면 모든 최소 조작이 계산된 것이므로 반복을 종료하고 count를 반환한다.
  */
 function solution(name) {
-  let count = 0;
-  let index = 0;
+  let count = 0; // 최소 커서 조작 횟수
+  let index = 0; // 현재 커서 위치
+  /**
+   * 각 문자에서 ▲, ▼ 방향으로 움직여야 하는 횟수
+   */
   const minCount = [...name].map((n) => {
     const fromA = n.charCodeAt() - "A".charCodeAt();
     const fromZ = "Z".charCodeAt() - n.charCodeAt() + 1;
     return Math.min(fromA, fromZ);
   });
+  /**
+   * 문자열의 좌측 끝에서 커서를 ◀ 방향으로 이동하면 문자열의 끝에 위치
+   * 문자열의 우측 끝에서 커서를 ▶ 방향으로 이동하면 문자열의 시작에 위치
+   */
   const wrap = (index) => {
-    if (index < 0) {
-      // index가 -1이고 name.length가 5일 때, 4를 반환
-      return name.length + index;
-    } else if (index >= name.length) {
-      // index가 5이고 length가 5일 때, 0을 반환
-      return index - name.length;
-    }
+    if (index < 0) return name.length + index;
+    if (index >= name.length) return index - name.length;
     return index;
   };
+  /**
+   * 덧셈 함수
+   */
   const sum = (numbers) => numbers.reduce((res, num) => res + num, 0);
 
   while (true) {
