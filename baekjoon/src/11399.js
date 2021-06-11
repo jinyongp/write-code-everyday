c1ea9f082da4ca0755cbb3a8f9246c71cccf1d93
jinyongp <dev.jinyongp@gmail.com>
2021-06-11 10:50:46 +0900

    Solve 11399 - js

---
+++ baekjoon/src/11399.js
@@ -0,0 +1,36 @@
+// https://www.acmicpc.net/problem/11399
+
+const readline = require("readline");
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const lines = [];
+
+/**
+ * P = [ 3, 1, 4, 3, 2 ];
+ * P를 정렬하면, [1, 2, 3, 3, 4]
+ * [1, 1+2, 1+2+3, 1+2+3+3, 1+2+3+3+4]
+ *
+ * P를 정렬하여 이중 반복문으로 필요한 시간을 계산한다.
+ *
+ * @param {number} N 사람의 수
+ * @param {number[]} P 인출하는데 걸리는 시간
+ * @return 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값
+ */
+function solution(N, P) {
+  P.sort((a, b) => a - b);
+  for (let i = 1; i < N; ++i) P[i] += P[i - 1];
+  return P.reduce((sum, num) => sum + num);
+}
+
+reader
+  .on("line", (line) => {
+    lines.push(line.trim());
+  })
+  .on("close", () => {
+    const N = Number(lines.shift());
+    const P = lines.shift().split(" ").map(Number);
+    console.log(solution(N, P));
+    process.exit();
+  });
