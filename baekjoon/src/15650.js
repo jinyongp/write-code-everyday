021a7f7bd903cb88acf7269749b8a0e8663444b2
jinyongp <dev.jinyongp@gmail.com>
2021-06-14 02:03:00 +0900

    Solve 15650 - js

---
+++ baekjoon/src/15650.js
@@ -0,0 +1,41 @@
+// https://www.acmicpc.net/problem/15650
+
+const readline = require("readline");
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+/**
+ *
+ *
+ * @param
+ * @return
+ */
+function solution(N, M) {
+  const numbers = Array.from({ length: N })
+    .fill(0)
+    .map((_, i) => i + 1);
+  return combinations(numbers, M);
+}
+
+function combinations(iterable, r = iterable.length) {
+  if (r === 1) return iterable.map((value) => [value]);
+  return iterable.reduce((result, value, index, origin) => {
+    const rest = origin.slice(index + 1);
+    return result.concat(combinations(rest, r - 1).map((c) => [value, ...c]));
+  }, []);
+}
+
+const lines = [];
+reader
+  .on("line", (line) => {
+    lines.push(line.trim());
+  })
+  .on("close", () => {
+    const [N, M] = lines.shift().split(" ").map(Number);
+    for (const numbers of solution(N, M)) {
+      console.log(numbers.join(" "));
+    }
+    process.exit();
+  });
